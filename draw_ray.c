fc4ebc8f88a9b62f6c21b51a657e3b33bbb1d1ad
Marius Meier <marius.meier24@gmail.com>
2024-10-14 16:36:07 +0300

    first version of checking for vertical and horizontal intersections

--- draw_ray.c
+++ draw_ray.c
@@ -6,7 +6,7 @@
 /*   By: mmeier <mmeier@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 16:40:26 by mmeier            #+#    #+#             */
-/*   Updated: 2024/10/11 16:19:36 by mmeier           ###   ########.fr       */
+/*   Updated: 2024/10/14 16:34:29 by mmeier           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,156 @@ int	create_pl_img(t_data *data)
 	return (0);
 }
 
+void	check_horizontal_inter(t_data *data)
+{
+	double	next_y;
+	double	next_x;
+	double	delta_y;
+	double	delta_x;
+	int		up;
+	
+	delta_x = 0;
+	delta_y = 0;
+	up = 0;
+	next_y = 0;
+	next_x = 0;
+	data->ray_dir_x = cos(data->p_a);
+	data->ray_dir_y = -sin(data->p_a);
+	printf("player y %f\n", data->y_p);
+	printf("player x %f\n", data->x_p);
+	if (data->ray_dir_y > 0) // ray moves upwards y-axis (South)
+	{
+		next_y = ceil(data->y_p); //calculates closest int upwards
+		printf("moves upwards\n");
+		printf("next_y %f\n", next_y);
+		up = 1;
+		delta_y = next_y - data->y_p;
+		next_x = data->x_p - delta_y / tan(data->p_a);
+		printf("next_x %f\n", next_x);
+	}
+	if (data->ray_dir_y < 0) // ray moves downwards y-axis (North))
+	{
+		next_y = floor(data->y_p); //calculates closest int downwards
+		printf("moves downwards\n");
+		printf("next_y %f\n", next_y);
+		up = 0;
+		delta_y = data->y_p - next_y;
+		next_x = data->x_p + delta_y / tan(data->p_a);
+		printf("next_x %f\n", next_x);
+	}
+	delta_x = 1 / fabs(tan(data->p_a));
+	while (1)
+	{
+		if (data->ray_dir_x > 0) // Ray moving to the right
+		{
+			if (up) // Moving upwards (visually downwards)
+			{
+				next_y += 1;
+				next_x += delta_x;
+			}
+			else // Moving downwards (visually upwards)
+			{
+				next_y -= 1;
+				next_x += delta_x;
+			}
+		}
+		else if (data->ray_dir_x < 0) // Ray moving to the left
+		{
+			if (up) // Moving upwards (visually downwards)
+			{
+				next_y += 1;
+				next_x -= delta_x;
+			}
+			else // Moving downwards (visually upwards)
+			{
+				next_y -= 1;
+				next_x -= delta_x;
+			}
+		}
+		printf("next y %f\n", next_y);
+		printf("next x %f\n", next_x);
+		if (next_y <= 0 || next_x <= 0 || next_y >= data->height
+			|| next_x >= ft_strlen(data->map[(int)next_y]) || data->map[(int)next_y][(int)next_x] == '1')
+			break ;
+	}
+}
+
+static void	check_vertical_inter(t_data *data)
+{
+	double	next_y;
+	double	next_x;
+	double	delta_y;
+	double	delta_x;
+	int		right;
+	
+	delta_x = 0;
+	delta_y = 0;
+	right = 0;
+	next_y = 0;
+	next_x = 0;
+	data->ray_dir_x = cos(data->p_a);
+	data->ray_dir_y = -sin(data->p_a);
+	printf("player y %f\n", data->y_p);
+	printf("player x %f\n", data->x_p);
+	if (data->ray_dir_x > 0) // ray moves right x-axis
+	{
+		next_x = ceil(data->x_p); //calculates closest int upwards
+		printf("moves right\n");
+		printf("next_x %f\n", next_x);
+		right = 1;
+		delta_x = next_x - data->x_p;
+		next_y = data->y_p - delta_x * tan(data->p_a);
+		printf("next_y %f\n", next_y);
+	}
+	if (data->ray_dir_x < 0) // ray moves left x-axis
+	{
+		next_x = floor(data->x_p); //calculates closest int downwards
+		printf("moves left\n");
+		printf("next_x %f\n", next_x);
+		right = 0;
+		delta_x = data->x_p - next_x;
+		next_y = data->y_p + delta_x * tan(data->p_a);
+		printf("next_y %f\n", next_y);
+	}
+	delta_y = fabs(delta_x * tan(data->p_a));
+	while (1)
+	{
+		if (data->ray_dir_y > 0) //ray moves upwards y-axis (South)
+		{
+			if (right)
+			{
+				next_x += 1;
+				next_y += delta_y;
+			}
+			else // Moving left
+			{
+				next_x -= 1;
+				next_y += delta_y;
+			}
+		}
+		else if (data->ray_dir_y < 0) //ray moves downwards y-axis (North)
+		{
+			if (right)
+			{
+				next_x += 1;
+				next_y -= delta_y;
+			}
+			else // Moving left
+			{
+				next_x -= 1;
+				next_y -= delta_y;
+			}
+		}
+		printf("next y %f\n", next_y);
+		printf("next x %f\n", next_x);
+		if (next_y <= 0 || next_x <= 0 || next_y >= data->height
+			|| next_x >= ft_strlen(data->map[(int)next_y]) || data->map[(int)next_y][(int)next_x] == '1')
+			break ;
+	}
+}
+
+
+
 /*Draws single ray from player's direction until wall. PXP/2 used
   in order to center ray onto the player. 'cos' and '-sin' set ray
   direction based on player's agnle. '-sin' as Y-axis inverted*/
@@ -39,6 +189,8 @@ void	draw_line(t_data *data)
 	int		y;
 	float	magnitude;
 
+	// check_horizontal_inter(data);
+	check_vertical_inter(data);
 	x = (int)(data->x_p * PX) + PXP / 2;
 	y = (int)(data->y_p * PX) + PXP / 2;
 	data->ray_dir_x = cos(data->p_a);
@@ -58,6 +210,8 @@ void	draw_line(t_data *data)
 		mlx_image_to_window(data->mlx, data->img->ray, 0, 0);
 	}
 }
+
+
 
 // void check_horizontal_inter(t_data *data)
 // {
