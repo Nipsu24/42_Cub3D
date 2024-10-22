4b80dae9dcf90dd3a57695f4ded8093ae81b147a
Marius Meier <marius.meier24@gmail.com>
2024-10-22 16:06:19 +0300

    first iteration on direction shades

--- draw_ray.c
+++ draw_ray.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   draw_ray.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lstorey <lstorey@student.42.fr>            +#+  +:+       +#+        */
+/*   By: mmeier <mmeier@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 16:40:26 by mmeier            #+#    #+#             */
-/*   Updated: 2024/10/22 15:06:16 by lstorey          ###   ########.fr       */
+/*   Updated: 2024/10/22 15:55:40 by mmeier           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -97,6 +97,56 @@ static float	draw_single_ray_3d(t_data *data, float angle)
 	}
 }
 
+static int	hit_direction(t_data *data, float angle)
+{
+	float	x;
+	float	y;
+	float	mag;
+	int		hit_dir;
+	int map_x, map_y;
+
+	x = data->x_p;
+	y = data->y_p;
+	data->ray_dir_x = cos(angle);
+	data->ray_dir_y = -sin(angle);
+	mag = sqrt(data->ray_dir_x * data->ray_dir_x
+			+ data->ray_dir_y * data->ray_dir_y);
+	data->ray_dir_x /= mag;
+	data->ray_dir_y /= mag;
+	while (1)
+	{
+		x += (data->ray_dir_x * ray_speed);
+		y += (data->ray_dir_y * ray_speed);
+		if (x < 0 || y < 0 || x >= screen_width || y >= screen_height
+			|| data->map[(int)y][(int)x] == '1')
+		{
+			map_x = (int)x;
+        	map_y = (int)y;
+			float x_diff = fabsf(x - map_x); // Distance to the vertical wall
+            float y_diff = fabsf(y - map_y); // Distance to the horizontal wall
+            
+            // If the ray is closer to the vertical grid line (x-axis), it's an East/West wall
+            if (x_diff > y_diff)
+            {
+                if (data->ray_dir_x > 0) 
+                    hit_dir = 2; // East-facing wall
+                else 
+                    hit_dir = 3; // West-facing wall
+            }
+            // If the ray is closer to the horizontal grid line (y-axis), it's a North/South wall
+            else
+            {
+                if (data->ray_dir_y > 0) 
+                    hit_dir = 4; // South-facing wall
+                else 
+                    hit_dir = 1; // North-facing wall
+            }
+			return(hit_dir);
+        }
+		
+	}
+}
+
 static void	updating_fg(t_data *data) //ORGINAL
 {
 	int		ray_index;
@@ -112,26 +162,29 @@ static void	updating_fg(t_data *data) //ORGINAL
 	ray_index = 0;
 
 	while (ray_index < rays)
-	{
-		len = data->img->len[ray_index];
-		if (len == 0)
-    		len = 0.01f;
-		line_height = (block_height) / len;
-		start_y = screen_center - (line_height / 2);
-		end_y = screen_center + (line_height / 2);
-		y = start_y;
-		pixel_x = screen_width - (ray_index * (screen_width / (float)rays));
-		while (y <= end_y)
-		{
-			if (y >= 0 && y < screen_height)
-			{
-				// printf("y: %f\n", y);
-				mlx_put_pixel(data->img->fg, pixel_x, y, 0xFFFFFFFF);
-			}
-			y += 1;
-		}
-		ray_index++;
-	}
+    {
+        len = data->img->len[ray_index];
+        if (len == 0)
+            len = 0.01f;
+        line_height = (block_height) / len;
+        start_y = screen_center - (line_height / 2);
+        end_y = screen_center + (line_height / 2);
+        y = start_y;
+        pixel_x = screen_width - (ray_index * (screen_width / (float)rays));
+        while (y <= end_y)
+        {
+            if (y >= 0 && y < screen_height && data->img->hit_dir[ray_index] == 1)
+                mlx_put_pixel(data->img->fg, pixel_x, y, 0x00FFFFFF);
+            if (y >= 0 && y < screen_height && data->img->hit_dir[ray_index] == 2)
+                mlx_put_pixel(data->img->fg, pixel_x, y, 0xFF00FFFF);
+            if (y >= 0 && y < screen_height && data->img->hit_dir[ray_index] == 3)
+                mlx_put_pixel(data->img->fg, pixel_x, y, 0xFFFF00FF);
+            if (y >= 0 && y < screen_height && data->img->hit_dir[ray_index] == 4)
+                mlx_put_pixel(data->img->fg, pixel_x, y, 0x000000FF);
+            y += 1;
+        }
+        ray_index++;
+    }
 	mlx_image_to_window(data->mlx, data->img->fg, 0, 0);	
 }
 
@@ -154,6 +207,7 @@ void	draw_fov_3d(t_data *data)
 	while (current_angle <= end_angle && i < rays)
 	{
 		data->img->len[i] = draw_single_ray_3d(data, current_angle);
+		data->img->hit_dir[i] = hit_direction(data, current_angle);
 		current_angle += step_angle;
 		i++;
 	}
