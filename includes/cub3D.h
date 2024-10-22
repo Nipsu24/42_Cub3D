4b5844c470e0f6603637e0715cb43aac798d600c
Marius Meier <marius.meier24@gmail.com>
2024-10-22 16:30:26 +0300

    update wall colour

--- includes/cub3D.h
+++ includes/cub3D.h
@@ -6,7 +6,7 @@
 /*   By: mmeier <mmeier@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/19 11:56:58 by lstorey           #+#    #+#             */
-/*   Updated: 2024/10/22 15:58:29 by mmeier           ###   ########.fr       */
+/*   Updated: 2024/10/22 16:29:12 by mmeier           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,9 +24,9 @@
 # define ro_speed 0.3
 # define PI 3.1415926535
 # define mm_rays 20
-# define rays 1000
+# define rays 1500
 # define screen_width 1280
-# define screen_height screen_width/2 // always determined by screen_width
+# define screen_height screen_width / 2 // always determined by screen_width
 # define mm_size 4 // downscale factor for mini map and player
 # define block_height screen_height / 2
 typedef struct s_data	t_data;
