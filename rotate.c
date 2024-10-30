769bdfddbbb3b35d91937bca8e904aecb9a27b7a
Marius Meier <mmeier@student.hive.fi>
2024-10-30 12:08:01 +0200

    adds raycaster with arc

--- rotate.c
+++ rotate.c
@@ -6,7 +6,7 @@
 /*   By: mmeier <mmeier@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 16:40:26 by mmeier            #+#    #+#             */
-/*   Updated: 2024/10/27 10:18:04 by mmeier           ###   ########.fr       */
+/*   Updated: 2024/10/30 10:55:18 by mmeier           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ void	rotate_left(t_data *data)
 	data->p_a += RO_SPEED;
 	if (data->p_a >= 2 * PI)
 		data->p_a -= 2 * PI;
+	if (data->p_a <= 0)
+		data->p_a += 2 * PI;
 	data->p_dx = cos(data->p_a);
 	data->p_dy = sin(data->p_a);
 	mlx_delete_image(data->mlx, data->img->ray);
@@ -28,8 +30,6 @@ void	rotate_left(t_data *data)
 	// draw_fov_3d(data);
 	build_map(data);
 	// draw_fov(data);
-	// check_closest_hor_inter(data);
-	// check_closest_ver_inter(data);
 	raycaster(data);
 }
 
@@ -38,8 +38,10 @@ void	rotate_left(t_data *data)
 void	rotate_right(t_data *data)
 {
 	data->p_a -= RO_SPEED;
-	if (data->p_a < 0)
+	if (data->p_a <= 0)
 		data->p_a += 2 * PI;
+	if (data->p_a >= 2 * PI)
+		data->p_a -= 2 * PI;
 	data->p_dx = cos(data->p_a);
 	data->p_dy = sin(data->p_a);
 	mlx_delete_image(data->mlx, data->img->ray);
@@ -49,7 +51,5 @@ void	rotate_right(t_data *data)
 	// draw_fov_3d(data);
 	build_map(data);
 	// draw_fov(data);
-	// check_closest_hor_inter(data);
-	// check_closest_ver_inter(data);
 	raycaster(data);
 }
