03ce8123980177b49166628135dc7be4962c3448
lstorey <leostorey85@hotmail.com>
2024-11-04 11:19:12 +0200

    Leo branch is dead! Long live Leo branch

--- includes/cub3D.h
+++ includes/cub3D.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cub3D.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mmeier <mmeier@student.hive.fi>            +#+  +:+       +#+        */
+/*   By: lstorey <lstorey@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/19 11:56:58 by lstorey           #+#    #+#             */
-/*   Updated: 2024/11/01 15:13:55 by mmeier           ###   ########.fr       */
+/*   Updated: 2024/11/04 11:16:59 by lstorey          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,14 @@
 # define STEPS 0.2f
 # define RO_SPEED 0.1f
 # define PI 3.1415926535
-# define S_WID 630
-# define S_HEI 630
+# define S_WID 1630
+# define S_HEI 1230
 # define MM_SIZE 4 // downscale factor for mini map and player
 # define B_HEI 630
 # define EPS 1e-6
 # define BLOCK_SIZE 1
 # define FOV (PI / 3) // 60 degrees
-# define RAYS_MODULO 80
+# define RAYS_MODULO 200
 
 typedef struct s_data	t_data;
 typedef struct s_img
